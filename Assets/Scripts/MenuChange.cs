6086920a41c2b1d2805231292cb0831877842d39
PC_Var <PC_Var@email.com>
2024-11-08 16:47:03 +0100

    Menu Close Game, Turn Speed and Scale Object

--- Assets/Scripts/MenuChange.cs
+++ Assets/Scripts/MenuChange.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
+using static System.Net.Mime.MediaTypeNames;
+using UnityEngine.SceneManagement;
 
 public class MenuChange : MonoBehaviour
 {
     public ContinuousMoveProviderBase ContinuousMoveProviderBase;
-    public Slider speedSlider;
-    public TextMeshProUGUI text;
+    public ContinuousTurnProviderBase ContinuousTurnProviderBase; // Imagino que es este
+    public Slider speedSlider; // Para movimiento personaje
+    public Slider turnSpeedSlider;  // Slider para la velocidad de giro
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI turnText;
 
     public Button spawnBanderaButton;
-    public GameObject bandera; //De momento es un cubo
+    public Button quitGameButton;  // Referencia al botón de salida
+    public GameObject bandera; //De momento es un cilindro
 
     public Transform player;
 
@@ -23,16 +29,21 @@ public class MenuChange : MonoBehaviour
             Debug.LogError("No hay ContinuousMoveProvider");
             return;
         }
-        if (ContinuousMoveProviderBase == null)
+        if (ContinuousTurnProviderBase == null)
         {
-            Debug.LogError("No hay ContinuousMoveProvider");
+            Debug.LogError("No hay ContinuousTurnProvider");
             return;
         }
 
+
         speedSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        turnSpeedSlider.onValueChanged.AddListener(OnTurnSpeedSliderValueChanged);
 
         speedSlider.value = ContinuousMoveProviderBase.moveSpeed;
         UpdateSpeedText(ContinuousMoveProviderBase.moveSpeed);
+
+        turnSpeedSlider.value = ContinuousTurnProviderBase.turnSpeed;
+        UpdateTurnText(ContinuousTurnProviderBase.turnSpeed);
 
         // Asegurarnos de que el botón BanderaSpawn esté configurado correctamente
         if (spawnBanderaButton != null)
@@ -43,16 +54,43 @@ public class MenuChange : MonoBehaviour
         {
             Debug.LogError("El botón BanderaSpawn no está asignado.");
         }
+
+        // Configuración del botón QuitGame
+        if (quitGameButton != null)
+        {
+            //Este para cerrar el juego
+            quitGameButton.onClick.AddListener(QuitGame);
+            
+            //Este para salir al menu principal
+            //quitGameButton.onClick.AddListener(GoToMainMenu);
+        }
+        else
+        {
+            Debug.LogError("El botón QuitGame no está asignado.");
+        }
     }
 
+    //Velocidad del player
     void OnSliderValueChanged(float value) { 
         ContinuousMoveProviderBase.moveSpeed = value;
         UpdateSpeedText(value);
     }
 
+    //Velocidad de giro del jugador
+    void OnTurnSpeedSliderValueChanged(float value){
+        ContinuousTurnProviderBase.turnSpeed = value;
+        UpdateTurnText(value);
+    }
+
+
     void UpdateSpeedText(float value)
     {
-        text.text = $"{value:F1}";
+        speedText.text = $"{value:F1}";
+    }
+
+    void UpdateTurnText(float value)
+    {
+        turnText.text = $"{value:F1}";
     }
 
     void SpawnCube()
@@ -71,4 +109,22 @@ public class MenuChange : MonoBehaviour
             Debug.LogError("No se puede generar el cubo, asegúrese de que el prefab y el jugador estén asignados.");
         }
     }
+
+    // Función para salir del juego
+    void QuitGame()
+    {
+        #if UNITY_EDITOR
+            // Si estamos en el editor, detenemos la simulación
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // Si estamos en una build del juego, cerramos la aplicación
+            Application.Quit();
+        #endif
+    }
+
+    void GoToMainMenu()
+    {
+        SceneManager.LoadScene("1 Start Scene");
+    }
+
 }
